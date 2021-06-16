37514f8c31311404596b384279660a54fd65ce4a
Sinh <trannguyensinh2004@gmail.com>
2021-06-16 15:51:40 +0700

    hmm

--- main.js
+++ main.js
@@ -1,29 +1,47 @@
-const {app, BrowserWindow, ipcMain} = require('electron');
-const path = require('path');
+// Modules to control application life and create native browser window
+const { app, BrowserWindow, Menu, ipcMain } = require('electron')
+const path = require('path')
 
-const createWindow = () => {
-    const mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-            preload: path.join(__dirname, 'preload.js')
-        }
-    });
 
-    mainWindow.loadFile(path.join(__dirname, '/src/index.html'))
 
-    app.whenReady().then(() => {
-        createWindow()
-      
-        app.on('activate', function () {
-          if (BrowserWindow.getAllWindows().length === 0) createWindow()
-        })
-    })
+function createWindow () {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    title: "Data Manager",
+    minHeight:500,
+    minWidth:600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+  Menu.setApplicationMenu(null)
+  mainWindow.loadFile('src/index.html')
 
-    app.on('window-all-closed', () => {
-        if (process.platform !== 'win32') app.quit();
-    })
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools()
 }
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', function () {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
 
 //Catch item:add
 const info = require('./database/information');
